d5ea54964ef2c78f304e9878e365d56992c72bd7
raaghavbajoria <211b235@juetguna.in>
2024-02-25 17:29:51 +0530

    created rest of the project

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDizB1HvdZtbjOYETwWIOPhKuh115ffThA",
+  authDomain: "homehub-728c3.firebaseapp.com",
+  projectId: "homehub-728c3",
+  storageBucket: "homehub-728c3.appspot.com",
+  messagingSenderId: "751500235567",
+  appId: "1:751500235567:web:af427c47c18b59527787da"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
